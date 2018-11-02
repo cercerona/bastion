b6e9eccc0423ad6412f987efbf6829bfedbe6f41
Dmitry Kononov <cercerona@gmail.com>
2018-11-03 01:55:02 +0300

    Модуль, содержащий функцию чтения отчета из csv файла-выгрузки

---
+++ report_system/read_csv_report_files.py
@@ -0,0 +1,8 @@
+"""Include functions for read csv files with WAB report"""
+def read_report(file_obj):
+    """
+    Function for read report from csv file
+    :param file_obj: file to read
+    :return: report in rows
+    """
+    pass  # FIXME
