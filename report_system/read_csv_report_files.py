cb24995291596117c68175c9609436fe2be4a8f4
Dmitry Kononov <cercerona@gmail.com>
2018-11-03 17:48:29 +0300

    Added a function to read a report from a file that returns a table with a report in rows.

--- report_system/read_csv_report_files.py
+++ report_system/read_csv_report_files.py
@@ -1,8 +1,21 @@
+import csv
+import pprint
+
 """Include functions for read csv files with WAB report"""
 def read_report(file_obj):
     """
     Function for read report from csv file
     :param file_obj: file to read
-    :return: report in rows
+    :return: table - report in table with rows of the file
     """
-    pass  # FIXME
+    return [row for row in csv.reader(file_obj)]
+
+if __name__ == '__main__':
+    file_name = r'C:\Users\Dmitry Kononov\Documents\Бастион\Python\Отчеты\Примеры выгрузок\БСпб\statistics\srv-wab-dca-2.bankspb.ru_2018-07-20T17_55_s_connections_per_device.csv'
+    table = []
+    with open(file_name, 'r') as infile:
+        table = read_report(infile)
+
+    pprint.pprint(table)
+    print('Всего записей: ', len(table))
+
