6877accf380562f1d61ab346ba1237aff2274878
Dmitry Kononov <cercerona@gmail.com>
2018-11-05 01:43:02 +0300

    Обновлен обработчик открытия файла с отчетом.

--- report_system/gui.py
+++ report_system/gui.py
@@ -1,25 +1,31 @@
 """File include the interface function for WAB report processing"""
+from read_csv_report_files import *
 import tkinter
 from tkinter import filedialog as fd
 
-def input_report_filename(event):
+def open_report_file(event):
     """
-    Функция возвращает имя файла с отчетом
-    :return: Имя файла с отчетом
+    Функция открывает файл с отчетом
     """
-    return fd.askopenfilename(filetype = [("Report files", "*.csv")])
+    table = []
+    with open(fd.askopenfilename(filetype = [("Report files", "*.csv")]), 'r') as infile:
+        table = read_report(infile)
+
+    pprint.pprint(table)
+    print('Всего записей: ', len(table))
+
 
 def init_main_window():
     """Создает и инициирует виджеты главного окна
     :param: нет
     :return: ничего
     """
-    global root, open_file_button #создание глобальных виджетов главного окна и кнопки открытия файла
+    global root, open_file_button, open_file_button #создание глобальных виджетов главного окна и кнопки открытия файла, имя файла отчета
     root = tkinter.Tk()
     root.title("WAB reports")
     open_file_button = tkinter.Button(root, text = 'Open report')
     open_file_button.pack()
-    open_file_button.bind('<Button>', input_report_filename)
+    open_file_button.bind('<Button>', open_report_file)
 
 if __name__ == '__main__':
     init_main_window()
